81b7b469449f436db920c168b8e56af8d1ffa479
Bocinsky <bocinsky@gmail.com>
2017-05-24 00:19:05 -0600

    FINAL SUBMISSION RUN

--- asian_niche.sh
+++ asian_niche.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## Set the version as an environment variable
 VERSION="0.9.0"
-ARCH_SITES="./DATA/DALPOIMGUEDES_BOCINSKY_2017.xlsx"
+ARCH_SITES="~/Dropbox/asian_niche/DALPOIMGUEDES_BOCINSKY_2017.xlsx"
 
 ## Build the Docker image from the github repo
 docker build -t bocinsky/asian_niche https://github.com/bocinsky/asian_niche.git#$VERSION
@@ -19,7 +19,7 @@ docker start asian_niche
 docker cp $ARCH_SITES asian_niche:/asian_niche/DATA/DALPOIMGUEDES_BOCINSKY_2017.xlsx
 
 ## Download and copy pre-run output into the container
-docker cp ~/Desktop/OUTPUT asian_niche:/asian_niche/
+#docker cp ~/Desktop/OUTPUT asian_niche:/asian_niche/
 
 ## Run the analysis in the docker container
 docker exec asian_niche Rscript asian_niche.R
